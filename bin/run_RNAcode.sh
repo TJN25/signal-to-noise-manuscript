8f86be2f5f86e0951517370422d90a87680b03e7
TJN25 <nicholson.tom.n@gmail.com>
2022-03-07 15:55:56 +1300

    Update to manuscript.Rmd,
    The methods section of manuscript.Rmd now need to have the final
    step of running the tools added to it, and the results section needs to
    be added.
    
    A data folder has been added, and all the data files need to be
    transfered over and the paths renamed.

--- bin/run_RNAcode.sh
+++ bin/run_RNAcode.sh
@@ -5,30 +5,27 @@
 
 usage(){
     echo "sraAlignAndFold.sh is a script for making a multiple sequence alignment and
-    getting secondary structure information.  
+    getting secondary structure information.
 Usage:
- fetchGenomeGCA.sh [opts] [input]
+ fetchGenomeGCA.sh -i <input_folder>
 
 Options:
+-i  Input folder where the alignment are in stk format (default is the working
+directory)
 	-h	Display this help
-
-Input	       
-	-r	Folder location
 
 "
 }
 
 while getopts "i:o:h" arg; do
 case $arg in
-	i) 
+	i)
 	FOLDER=${OPTARG};;
-	o) 
-	OUTPUT=${OPTARG};;
     h)
 		usage
 		exit
-      ;;    
-	\?) 
+      ;;
+	\?)
 	echo "Unknown option: -${OPTARG}" >&2; exit 1;;
     esac
 done
@@ -37,56 +34,37 @@ if [ -z ${FOLDER} ]; then
 	FOLDER="./"
 fi
 
-
-
-
 mkdir -p "$FOLDER/rnacode_out"
 
-
-
 let "fileNum = 0"
-for file in alignments/*.stk; #change to _R for positive control
+for file in alignments/*.stk;
 do
 
+#Check if the file has already been created. Used for cases where the
+#program crashed part way through
 checkname=`basename $file .stk`
 if [ -f "./rnacode_out/${checkname}.rnacode" ]; then
-echo "Already exists: $file"
-continue
+    echo "Already exists: $file"
+    continue
 else
+    #get number of sequences and length of sequences
+    nseqs=`esl-alistat $file | grep "Number of sequences" | cut -d ":" -f2`
 
-	
-nseqs=`esl-alistat $file | grep "Number of sequences" | cut -d ":" -f2`
+    length=`esl-alistat $file | grep "Alignment length:" | cut -d ":" -f2`
+    largest_length=`esl-alistat $file | grep "Largest" | cut -d ":" -f2`
 
-length=`esl-alistat $file | grep "Alignment length:" | cut -d ":" -f2`
-largest_length=`esl-alistat $file | grep "Largest" | cut -d ":" -f2`
+    #check the sequences are <500 nt long
+    if (( $length < 500 )); then
+        diffLength=`expr $largest_length - $length`
+        #check that the alignment is not poor
+        if (( $diffLength > $length ));then
+            echo "Alignment is poor: $file"
+            continue
+        fi
 
+        echo "Running RNAcode on $file (length: $length, nseqs: $nseqs)"
+        esl-reformat clustal $file | RNAcode --outfile ./rnacode_out/$checkname.rnacode
 
-if (( $length < 500 )); then
-
-diffLength=`expr $largest_length - $length`
-
-if (( $diffLength > $length ));then
-echo "Alignment is poor: $file"
-continue
+    fi
 fi
-
-if (( $nseqs > 5000 )); then
-echo "Skipping $file (length: $length, nseqs: $nseqs)"
-echo "$file (length: $length, nseqs: $nseqs)" >> skipped_alignments.txt
-continue
-fi
-
-if (( $nseqs < 3 )); then
-echo "Skipping $file (length: $length, nseqs: $nseqs)"
-#echo "$file (length: $length, nseqs: $nseqs)" >> skipped_alignments.txt
-continue
-fi
-
-echo "Running RNAcode on $file (length: $length, nseqs: $nseqs)"
-	
-esl-reformat clustal $file | RNAcode --outfile ./rnacode_out/$checkname.rnacode
-
-fi
-fi
-
 done
